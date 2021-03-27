fe728c1af36642b6f904e73360b3b00bed8dc164
shuheishintani <shuheishintani.dev@gmail.com>
2021-03-27 22:19:52 +0900

    fix

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
 import { bindings } from "@/bindings";
+import { CustomError } from "@/fragments/CustomError";
+import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
+import helmet from "helmet";
 import { Container } from "inversify";
 import { InversifyExpressServer } from "inversify-express-utils";
 import "reflect-metadata";
@@ -19,6 +22,23 @@ import { config } from "./express";
     },
     app
   );
+  server.setErrorConfig((app) => {
+    app.use(
+      (
+        err: CustomError,
+        _req: express.Request,
+        res: express.Response,
+        _next: express.NextFunction
+      ) => {
+        console.error(err.stack);
+        console.log(err.message);
+        res.status(err.statusCode || 500);
+        res.send({
+          error: { status: err.statusCode || 500, message: err.message },
+        });
+      }
+    );
+  });
   const buildServer = server.build();
   buildServer.listen(process.env.PORT || 3000, () => {
     console.log("Server is listening on port 3000");
